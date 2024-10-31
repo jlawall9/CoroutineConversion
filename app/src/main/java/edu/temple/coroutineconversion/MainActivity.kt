bf32a4b49f457bd338734e8107dbb4e9b2e8ecf0
jlawall9 <johnlawall24@gmail.com>
2024-10-31 12:05:40 -0400

    Done! Happy halloween

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -7,10 +7,16 @@ import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
-
+////////////////////////////////////////////viewPager.adapter.notifyDataSetChanged
     //TODO (Refactor to replace Thread code with coroutines)
 
     private val cakeImageView: ImageView by lazy {
@@ -21,24 +27,46 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.currentTextView)
     }
 
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
-
-        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
-        cakeImageView.alpha = it.what / 100f
-        true
-    })
+//    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
+//
+//        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
+//        cakeImageView.alpha = it.what / 100f
+//        true
+//    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
-                }
-            }.start()
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
+
+        findViewById<Button>(R.id.revealButton).setOnClickListener {
+            scope.launch {
+                repeater()
+
+            }
+        }
+
+
+//        findViewById<Button>(R.id.revealButton).setOnClickListener{
+//            Thread{
+//                repeat(100) {
+//                    handler.sendEmptyMessage(it)
+//                    Thread.sleep(40)
+//                }
+//            }.start()
+//        }
+    }
+    suspend fun repeater() {
+        repeat(100) {
+            withContext(Dispatchers.Main) {
+
+                currentTextView.text =
+                    String.format(Locale.getDefault(), "Current opacity: %d", it)
+                cakeImageView.alpha = it / 100f
+
+
+            delay(40)}
         }
     }
 }
